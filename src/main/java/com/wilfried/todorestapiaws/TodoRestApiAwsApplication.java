bf802d2fd738d4ba4619031e2d95e8ab1daa22b4
wilfriedpapt89 <wilfriedpapt@gmail.com>
2021-11-11 14:51:37 +0200

    Initial commit

---
+++ src/main/java/com/wilfried/todorestapiaws/TodoRestApiAwsApplication.java
@@ -0,0 +1,13 @@
+package com.wilfried.todorestapiaws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoRestApiAwsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodoRestApiAwsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wilfried/todorestapiaws/TodoRestApiAwsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wilfried.todorestapiaws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoRestApiAwsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
